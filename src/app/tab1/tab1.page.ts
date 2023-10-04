5416de49c38f4a199eb93e34bf4150476d403bf1
Kaio Roberto <132502264+KaioRoberto@users.noreply.github.com>
2023-10-04 10:22:37 -0300

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-
 export class Tab1Page {
 
-  constructor() {}
+  toggle1: boolean = false;
+  toggle2: boolean = false;
+  toggle3: boolean = false;
+  inputText: string = '';
+  peso: number; 
+
+  calcularDosagem() {
+    console.log('Calculando dosagem...');
+    console.log('Toggle 1:', this.toggle1);
+    console.log('Toggle 2:', this.toggle2);
+    console.log('Toggle 3:', this.toggle3);
+    console.log('Texto Inserido:', this.inputText);
+
+    if (this.peso < 0) {
+      console.log('Peso não pode ser negativo. Definindo como zero.');
+      this.peso = 0;
+    }
+
+    console.log('Peso:', this.peso);
+  }
+
+  toggleInput() {
+    if (!this.toggle1) {
+      this.inputText = ''; 
+    }
+  }
 }
